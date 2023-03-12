726bb2fa5202f81f968e2f69e738d9fb8a4626c4
Frank Huang <karokwbh2000@gmail.com>
2023-03-12 21:36:25 +0800

    init

---
+++ app.js
@@ -0,0 +1,10 @@
+// 編號：CANDY-007
+// 程式語言：JavaScript
+// 題目：在某個數字陣列裡，可能藏有某個不合群的奇數或偶數，試著找出它！
+
+function findSomeDifferent(numbers) {
+    // 實作寫在這裡
+}
+
+console.log(findSomeDifferent([2, 4, 0, 100, 4, 11, 2602, 36])) // 印出 11
+console.log(findSomeDifferent([160, 3, 1719, 19, 11, 13, -21])) // 印出 160
